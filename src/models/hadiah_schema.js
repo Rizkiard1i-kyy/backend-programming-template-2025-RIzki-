45a146cd5b041af73cc84a07308a2165c9e51620
rizkiard1-kyy <rizkiardi405@gmail.com>
2026-04-12 15:45:56 +0700

    fix hadiah-schema

--- src/models/hadiah_schema.js
+++ src/models/hadiah_schema.js
@@ -1,10 +1,10 @@
 module.exports = (mongoose) => {
-  const prizeSchema = new mongoose.Schema({
+  const hadiahSchema = new mongoose.Schema({
     nama: { 
         type: String, 
         required: true 
     },
-    
+
     maxWinners: { 
         type: Number, 
         required: true 
